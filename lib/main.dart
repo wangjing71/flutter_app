cdb37c1287414b899d5577ac8dc5f17edfc663ef
397383523@qq.com <8362781wj>
2019-04-22 22:20:36 +0800

    Container 容器

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,25 @@ class MyApp extends StatelessWidget {
             title: Text('TextWidget'),
           ),
           body: Center(
-            child: Text(
-              '一切先从Jetpack说起。我在去年年底的时候抽时间研究了下Google的Jetpack，这是一款在18年Google I/O大会上推出的Android开发组件工具集，旨在帮助我们轻松构建更稳定、更健壮、以及更可维护的应用程序。',
-              textAlign: TextAlign.left,
-//              maxLines: 2,
-//              overflow: TextOverflow.fade,
-            style: TextStyle(
-              fontSize: 25.0,
-              color: Color.fromARGB(255, 255, 150, 150),
-              decoration: TextDecoration.underline,
-              decorationStyle: TextDecorationStyle.solid,
-            ),
+            child: Container(
+              child: new Text(
+                "Hello World",
+                style: TextStyle(fontSize: 40.0),
+              ),
+              alignment: Alignment.center,
+              width: 500,
+              height: 400,
+              padding: const EdgeInsets.fromLTRB(10, 20, 30, 40),
+              margin: const EdgeInsets.fromLTRB(10, 20, 30, 40),
+              decoration: new BoxDecoration(
+                gradient: const LinearGradient(
+                    colors: [
+                      Colors.greenAccent,
+                      Colors.purple,
+                      Colors.red,
+                    ]
+                )
+              ),
             ),
           ),
         ));
