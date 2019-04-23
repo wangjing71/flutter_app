445ffacc3e32b8df8044c384da6d51c23c820acd
397383523@qq.com <8362781wj>
2019-04-23 17:11:22 +0800

    Row

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,92 @@
 import 'package:flutter/material.dart';
+void main () => runApp(MyApp());
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context ){
+    var card = new Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title:new Text('吉林省吉林市昌邑区',style: TextStyle(fontWeight: FontWeight.w500),),
+            subtitle: new Text('技术胖:1513938888'),
+            leading: new Icon(Icons.account_box,color: Colors.lightBlue,),
+          ),
+          new Divider(),
+          ListTile(
+            title:new Text('北京市海淀区中国科技大学',style: TextStyle(fontWeight: FontWeight.w500),),
+            subtitle: new Text('胜宏宇:1513938888'),
+            leading: new Icon(Icons.account_box,color: Colors.lightBlue,),
+          ),
+          new Divider(),
+          ListTile(
+            title:new Text('河南省濮阳市百姓办公楼',style: TextStyle(fontWeight: FontWeight.w500),),
+            subtitle: new Text('JSPang:1513938888'),
+            leading: new Icon(Icons.account_box,color: Colors.lightBlue,),
+          ),
+          new Divider(),
+          ListTile(
+            title:new Text('河南省濮阳市百姓办公楼',style: TextStyle(fontWeight: FontWeight.w500),),
+            subtitle: new Text('JSPang:1513938888'),
+            leading: new Icon(Icons.account_box,color: Colors.lightBlue,),
+          ),
+          new Divider(),
+          ListTile(
+            title:new Text('河南省濮阳市百姓办公楼',style: TextStyle(fontWeight: FontWeight.w500),),
+            subtitle: new Text('JSPang:1513938888'),
+            leading: new Icon(Icons.account_box,color: Colors.lightBlue,),
+          ),
+          new Divider(),
+          ListTile(
+            title:new Text('河南省濮阳市百姓办公楼',style: TextStyle(fontWeight: FontWeight.w500),),
+            subtitle: new Text('JSPang:1513938888'),
+            leading: new Icon(Icons.account_box,color: Colors.lightBlue,),
+          ),
+          new Divider(),
+          ListTile(
+            title:new Text('河南省濮阳市百姓办公楼',style: TextStyle(fontWeight: FontWeight.w500),),
+            subtitle: new Text('JSPang:1513938888'),
+            leading: new Icon(Icons.account_box,color: Colors.lightBlue,),
+          ),
+          new Divider(),
+          ListTile(
+            title:new Text('河南省濮阳市百姓办公楼',style: TextStyle(fontWeight: FontWeight.w500),),
+            subtitle: new Text('JSPang:1513938888'),
+            leading: new Icon(Icons.account_box,color: Colors.lightBlue,),
+          ),
+          new Divider(),
+          ListTile(
+            title:new Text('河南省濮阳市百姓办公楼',style: TextStyle(fontWeight: FontWeight.w500),),
+            subtitle: new Text('JSPang:1513938888'),
+            leading: new Icon(Icons.account_box,color: Colors.lightBlue,),
+          ),
+          new Divider(),
+          ListTile(
+            title:new Text('河南省濮阳市百姓办公楼',style: TextStyle(fontWeight: FontWeight.w500),),
+            subtitle: new Text('JSPang:1513938888'),
+            leading: new Icon(Icons.account_box,color: Colors.lightBlue,),
+          ),
+          new Divider(),
+          ListTile(
+            title:new Text('河南省濮阳市百姓办公楼',style: TextStyle(fontWeight: FontWeight.w500),),
+            subtitle: new Text('JSPang:1513938888'),
+            leading: new Icon(Icons.account_box,color: Colors.lightBlue,),
+          ),
+          new Divider(),
+
+        ],
+      ),
+
+    );
+
+
     return MaterialApp(
-      title: 'ListView widget',
-      home: Scaffold(
-        appBar: new AppBar(
-          title: new Text('垂直方向布局'),
+      title:'ListView widget',
+      home:Scaffold(
+        appBar:new AppBar(
+          title:new Text('卡片布局'),
         ),
-        body: Center(
-            child: new Stack(
-          alignment: const FractionalOffset(0.5, 0.8),
-          children: <Widget>[
-            new CircleAvatar(
-              backgroundImage: new NetworkImage(
-                  'https://avatar.csdn.net/8/B/B/3_sinyu890807.jpg'),
-              radius: 100.0,
-            ),
-            new Container(
-              decoration: new BoxDecoration(
-                color: Colors.lightBlue,
-              ),
-              padding: EdgeInsets.all(5.0),
-              child: new Text('我是汪京啊'),
-            )
-          ],
-        )),
+        body:Center(child:card),
       ),
     );
   }
