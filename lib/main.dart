ad540ba18bc8b437836854a874108e593f687db6
397383523@qq.com <8362781wj>
2019-04-23 15:34:13 +0800

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,29 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context ){
+  Widget build(BuildContext context){
     return MaterialApp(
-      title:'Text widget',
+      title:'JSPang Flutter Demo',
       home:Scaffold(
-        body:Center(
-          child:Container(
-            child:new Image.network(
-              'https://www.baidu.com/img/bd_logo1.png?where=super',
-              scale:1.0,
-              fit: BoxFit.scaleDown,
-              repeat: ImageRepeat.repeat,
-            ),
-            width:300.0,
-            height:200.0,
-            color: Colors.lightBlue,
+          appBar:new AppBar(
+              title:new Text('ListView Widget')
           ),
+        body: new ListView(
+            scrollDirection: Axis.horizontal,
+            children:<Widget>[
+              new Image.network(
+                  'http://attachments.gfan.net.cn/forum/attachments2/201301/27/202158zbvrbafqa99vzcr5.jpg'
+              ),
+              new Image.network(
+                  'http://attachments.gfan.net.cn/forum/attachments2/201301/27/202510wrq4iaiqaryii5qg.jpg'
+              ),
+              new Image.network(
+                  'http://attachments.gfan.net.cn/forum/attachments2/201301/27/2025320az940ffaw2jv42z.jpg.thumb.jpg'
+              ),
+              new Image.network(
+                  'http://pic1.win4000.com/wallpaper/2018-07-02/5b39bf2435484.jpg'
+              )
+            ]
         ),
       ),
     );
