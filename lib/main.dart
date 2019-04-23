394125fd23c810c46d32306c6f64c1ef08d834b9
397383523@qq.com <8362781wj>
2019-04-23 15:57:09 +0800

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/MyList.dart';
+void main () => runApp(MyApp(
+    items: new List<String>.generate(1000, (i)=> "Item $i")
+));
 
-void main() => runApp(MyApp());
+class MyApp extends StatelessWidget{
 
-class MyApp extends StatelessWidget {
   final List<String> items;
   MyApp({Key key, @required this.items}):super(key:key);
   @override
