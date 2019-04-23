ecc60001351f3b5c36df4ec79223f9a369dcf54d
397383523@qq.com <8362781wj>
2019-04-23 17:05:52 +0800

    Row

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
-void main () => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context ){
-
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title:'ListView widget',
-
-      home:Scaffold(
-        appBar:new AppBar(
-          title:new Text('垂直方向布局'),
+      title: 'ListView widget',
+      home: Scaffold(
+        appBar: new AppBar(
+          title: new Text('垂直方向布局'),
         ),
-        body:Center(child:new Stack(
+        body: Center(
+            child: new Stack(
           alignment: const FractionalOffset(0.5, 0.8),
           children: <Widget>[
             new CircleAvatar(
-              backgroundImage: new NetworkImage('https://avatar.csdn.net/8/B/B/3_sinyu890807.jpg'),
+              backgroundImage: new NetworkImage(
+                  'https://avatar.csdn.net/8/B/B/3_sinyu890807.jpg'),
               radius: 100.0,
             ),
             new Container(
