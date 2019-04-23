017cab93a9872fe91799742708943d35345529e6
397383523@qq.com <8362781wj>
2019-04-23 17:25:58 +0800

    Row

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+void main () => runApp(MaterialApp(
+    title:'导航演示1',
+    home:new FirstScreen()
+));
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class FirstScreen extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return new Scaffold(
-        appBar: AppBar(title: Text('导航页面')),
-        body: Center(
-            child: RaisedButton(
-          child: Text('查看商品详情页面'),
-          onPressed: () {
-            Navigator.push(
-                context,
-                new MaterialPageRoute(
-                    builder: (context) => new SecondScreen()));
-          },
-        )));
+        appBar: AppBar(title:Text('导航页面')),
+        body:Center(
+            child:RaisedButton(
+              child:Text('查看商品详情页面'),
+              onPressed: (){
+                Navigator.push(context,new  MaterialPageRoute(
+                    builder:(context) =>new SecondScreen())
+                );
+              },
+            )
+        )
+    );
   }
 }
 
