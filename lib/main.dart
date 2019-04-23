a181b6488c468e4b793d7aedddd2cab30f8490f8
397383523@qq.com <8362781wj>
2019-04-23 17:40:43 +0800

    页面跳转

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,63 @@
 import 'package:flutter/material.dart';
-void main () => runApp(MaterialApp(
-    title:'导航演示1',
-    home:new FirstScreen()
-));
 
-class FirstScreen extends StatelessWidget{
+//传递的数据结构,也可以理解为对商品数据的抽象
+class Product{
+  final String title;  //商品标题
+  final String description;  //商品描述
+  Product(this.title,this.description);
+}
+
+void main(){
+  runApp(MaterialApp(
+      title:'数据传递案例',
+      home:ProductList(
+        products:List.generate(
+            20,
+                (i)=>Product('商品 $i','这是一个商品详情，编号为:$i')
+        ),
+      )
+  ));
+}
+
+class ProductList extends StatelessWidget{
+  final List<Product> products;
+  ProductList({Key key,@required this.products}):super(key:key);
   @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-        appBar: AppBar(title:Text('导航页面')),
-        body:Center(
-            child:RaisedButton(
-              child:Container(
-                child: new Text('查看商铺1'),
-              ),
-//              onPressed: (){
-//                Navigator.push(context,new  MaterialPageRoute(
-//                    builder:(context) =>new SecondScreen())
-//                );
-//              },
-            onPressed: (){
-                Navigator.push(context, new MaterialPageRoute(builder: (context)=>new SecondScreen()));
-            },
-            )
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title:Text('商品列表')),
+        body:ListView.builder(
+          itemCount:products.length,
+          itemBuilder: (context,index){
+            return ListTile(
+                title:Text(products[index].title),
+                onTap:(){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder:(context)=>new ProductDetail(product:products[index])
+                      )
+                  );
+                }
+            );
+          },
         )
     );
   }
 }
 
-class SecondScreen extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-        appBar:AppBar(title:Text('技术胖商品详情页')),
-        body:Center(child:RaisedButton(
-            child:RaisedButton(
-              child:Text('返回'),
-              onPressed: (){
-                Navigator.pop(context);
-              },
-            )
-        ))
-    );
+class ProductDetail extends StatelessWidget {
+  final Product product;
+  ProductDetail({Key key ,@required this.product}):super(key:key);
 
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: AppBar(
+          title:Text('${product.title}'),
+        ),
+        body:Center(child: Text('${product.description}'),)
+    );
   }
 }
-
-
