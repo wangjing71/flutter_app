cff46c2c77e4a9b14c8f5ee9c8eb8a944a9b89fd
397383523@qq.com <8362781wj>
2019-04-23 16:59:48 +0800

    Row

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,30 @@ void main () => runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context ){
+
     return MaterialApp(
       title:'ListView widget',
 
       home:Scaffold(
-          appBar:new AppBar(
-            title:new Text('垂直方向布局'),
-          ),
-          body:Column(
-            children: <Widget>[
-              Text('I am JSPang'),
-              Text('my website is jspang.com'),
-              Text('I love coding')
-            ],
-            crossAxisAlignment: CrossAxisAlignment.start,
-          )
+        appBar:new AppBar(
+          title:new Text('垂直方向布局'),
+        ),
+        body:Center(child:new Stack(
+          alignment: const FractionalOffset(0.5, 0.8),
+          children: <Widget>[
+            new CircleAvatar(
+              backgroundImage: new NetworkImage('https://avatar.csdn.net/8/B/B/3_sinyu890807.jpg'),
+              radius: 100.0,
+            ),
+            new Container(
+              decoration: new BoxDecoration(
+                color: Colors.lightBlue,
+              ),
+              padding: EdgeInsets.all(5.0),
+              child: new Text('我是汪京啊'),
+            )
+          ],
+        )),
       ),
     );
   }
