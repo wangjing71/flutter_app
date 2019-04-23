ba1650120ecdd10b79d4dd491cc4c41734cb4011
397383523@qq.com <8362781wj>
2019-04-23 15:50:44 +0800

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,22 @@ import 'package:flutter_app/MyList.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final List<String> items;
+  MyApp({Key key, @required this.items}):super(key:key);
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context ){
     return MaterialApp(
-      title: 'JSPang Flutter Demo',
-      home: Scaffold(
-          appBar: new AppBar(title: new Text('ListView Widget')),
-          body: Center(
-            child: Container(height: 200.0, child: MyList()),
-          )),
+      title:'ListView widget',
+      home:Scaffold(
+          body:new ListView.builder(
+              itemCount:items.length,
+              itemBuilder:(context,index){
+                return new ListTile(
+                  title:new Text('${items[index]}'),
+                );
+              }
+          )
+      ),
     );
   }
 }
