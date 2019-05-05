1a99a018db4151573230be98d02c15408e9995f6
397383523@qq.com <8362781wj>
2019-05-05 10:55:25 +0800

    传递数据

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,55 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Welcome to flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to flutter'),
+      home: new MyHomePage(
+        title: 'Weleome to flutter',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int counter = 0;
+
+  void incrementCount() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text('Weleome to flutter1'),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text('你点击的次数为：'),
+            new Text('$counter'),
+          ],
         ),
-        body: Center(
-          child: Text('Hellow Word'),
-        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: incrementCount,
+        child: new Icon(Icons.add),
+        tooltip: 'Increment',
       ),
     );
   }
