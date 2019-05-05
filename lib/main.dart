c7f5e11b645cb19e007f1d4a2a69e4c11508d344
397383523@qq.com <8362781wj>
2019-05-05 17:35:09 +0800

    表单

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,32 @@ import 'package:flutter/material.dart';
 
 //void main() => runApp(MyApp());
 void main() {
-  return runApp(MyApp());
+  return runApp(loginPage());
 }
 
-class MyApp extends StatelessWidget {
+class loginPage extends StatefulWidget {
+  @override
+  _loginPageState createState() => _loginPageState();
+}
+
+class _loginPageState extends State<loginPage> {
+  GlobalKey<FormState> loginKey = new GlobalKey<FormState>();
+
+  String userName;
+  String passWord;
+
+  void login(){
+    var loginForm = loginKey.currentState;
+    if(loginForm.validate()){
+      loginForm.save();
+      print('userName:'+userName+",passWord:"+passWord);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      title: 'flutter demo',
-      theme: ThemeData(
-          brightness: Brightness.light, // 应用 序整体主题的亮度
-          primaryColor: Colors.lightGreen[600], // App 主要部分的背景色
-          accentColor: Colors.orange[600]), //前景色（文本、按钮等）
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to flutter'),
-        ),
-        body: Center(
-          child: new RaisedButton(
-            onPressed: (){
-              
-            },
-            child: new Text('点击了按钮'),
-          )
-        ),
-      ),
-    );
+    return Container();
   }
 }
+
+
