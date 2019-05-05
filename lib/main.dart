1a4e5b09487217a65f090f2e5e7569e82dcafcb5
397383523@qq.com <8362781wj>
2019-05-05 10:20:57 +0800

    传递数据

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to flutter',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600]
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to flutter'),
