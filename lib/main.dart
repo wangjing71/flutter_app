05e2448de4b9ea519d796ae6275ed9ccc713b000
397383523@qq.com <8362781wj>
2019-05-05 10:37:41 +0800

    传递数据

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,14 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final appName = '自定义主题';
+
     return MaterialApp(
       title: 'Welcome to flutter',
       theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.lightBlue[800],
-        accentColor: Colors.cyan[600]
-      ),
+          brightness: Brightness.light,// 应用 序整体主题的亮度
+          primaryColor: Colors.lightGreen[600], // App 主要部分的背景色
+          accentColor: Colors.orange[600]), //前景色（文本、按钮等）
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to flutter'),
