d589e98b1a8952a869cd275a48077c1cd8a7fcd2
397383523@qq.com <8362781wj>
2019-05-05 17:12:34 +0800

    传递数据

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           brightness: Brightness.light, // 应用 序整体主题的亮度
           primaryColor: Colors.blue[600], // App 主要部分的背景色
-          accentColor: Colors.orange[600]), //前景色（文本、按钮等）
+      ), //前景色（文本、按钮等）
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to flutter'),
