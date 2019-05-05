1bdac3b757a356b45d75dc2b8ed8d6dcfa662004
397383523@qq.com <8362781wj>
2019-05-05 10:04:33 +0800

    传递数据

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,20 @@
 import 'package:flutter/material.dart';
 
-//传递的数据结构,也可以理解为对商品数据的抽象 1
-class Product{
-  final String title;  //商品标题
-  final String description;  //商品描述
-  Product(this.title,this.description);
-}
+void main() => runApp(MyApp());
 
-void main(){
-  runApp(MaterialApp(
-      title:'数据传递案例',
-      home:ProductList(
-        products:List.generate(
-            50, (i)=>Product('商品 $i','这是一个商品详情，编号为:$i')
-        ),
-      )
-  ));
-}
-
-class ProductList extends StatelessWidget{
-  final List<Product> products;
-  ProductList({Key key,@required this.products}):super(key:key);
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title:Text('商品列表22')),
-        body:ListView.builder(
-          itemCount:products.length,
-          itemBuilder: (context,index){
-            return ListTile(
-                title:Text(products[index].title),
-                onTap:(){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder:(context)=>new ProductDetail(product:products[index])
-                      )
-                  );
-                }
-            );
-          },
-        )
-    );
-  }
-}
-
-class ProductDetail extends StatelessWidget {
-  final Product product;
-  ProductDetail({Key key ,@required this.product}):super(key:key);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
+    return MaterialApp(
+      title: 'Welcome to flutter',
+      home: Scaffold(
         appBar: AppBar(
-          title:Text('${product.title}'),
+          title: Text('Welcome to flutter'),
         ),
-        body:Center(child: Text('${product.description}'),)
+        body: Center(
+          child: Text('Hellow Word'),
+        ),
+      ),
     );
   }
 }
