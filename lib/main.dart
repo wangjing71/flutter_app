dbf9faf5ede950471b255eaa6bb362aba62b308e
397383523@qq.com <8362781wj>
2019-05-05 18:00:20 +0800

    表单

--- lib/main.dart
+++ lib/main.dart
@@ -16,18 +16,69 @@ class _loginPageState extends State<loginPage> {
   String userName;
   String passWord;
 
-  void login(){
+  void login() {
     var loginForm = loginKey.currentState;
-    if(loginForm.validate()){
+    if (loginForm.validate()) {
       loginForm.save();
-      print('userName:'+userName+",passWord:"+passWord);
+      print('userName:' + userName + ",passWord:" + passWord);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Form表单示例',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Form表单示例'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: loginKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: '请输入用户名',
+                      ),
+                      onSaved: (value) {
+                        userName = value;
+                      },
+                      onFieldSubmitted: (value) {},
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: '请输入密码',
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        return value.length < 6 ? "密码长度不够6位" : null;
+                      },
+                      onSaved: (value) {
+                        passWord = value;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 340.0,
+              height: 42.0,
+              child: RaisedButton(
+                onPressed: login,
+                child: Text(
+                  '登录',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
-
-
