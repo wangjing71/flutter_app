7be75cd0fcd51185d5445ba687f9f6f80fd05aad
397383523@qq.com <8362781wj>
2019-05-06 16:35:24 +0800

    表单

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'flutter demo',
       theme: ThemeData(
@@ -19,12 +18,35 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to flutter'),
         ),
         body: Center(
-          child: new RaisedButton(
-            onPressed: (){
-              
-            },
-            child: new Text('点击了按钮'),
-          )
+//          child: SimpleDialog(
+//            title: Text('对话框标题'),
+//            children: <Widget>[
+//              SimpleDialogOption(
+//                child: Text("111111111"),
+//              ),
+//              SimpleDialogOption(
+//                child: Text("222222222"),
+//              ),
+//            ],
+//          ),
+
+          child: AlertDialog(
+            title: Text('提示'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text('11111111'),
+                  Text('2222222222'),
+                  Text('33333333333'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(onPressed: null, child: Text('确定',style: TextStyle(color: Colors.blue),)),
+              FlatButton(onPressed: null, child: Text('取消',style: TextStyle(color: Colors.blue))),
+
+            ],
+          ),
         ),
       ),
     );
