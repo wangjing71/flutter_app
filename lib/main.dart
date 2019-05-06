57bce4a9f97f85a7d7c89bb61bfaf018b106bc55
397383523@qq.com <8362781wj>
2019-05-06 09:30:52 +0800

    表单

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'flutter demo',
       theme: ThemeData(
-          brightness: Brightness.light, // 应用 序整体主题的亮度
-          primaryColor: Colors.lightGreen[600], // App 主要部分的背景色
-          accentColor: Colors.orange[600]), //前景色（文本、按钮等）
+          primarySwatch: Colors.blue,
+//          brightness: Brightness.light, // 应用 序整体主题的亮度
+//          primaryColor: Colors.lightGreen[600], // App 主要部分的背景色
+//          accentColor: Colors.orange[600] //前景色（文本、按钮等）
+          ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to flutter'),
         ),
         body: Center(
-          child: new RaisedButton(
-            onPressed: (){
-              
-            },
-            child: new Text('点击了按钮'),
-          )
-        ),
+            child: new RaisedButton(
+          onPressed: () {},
+          child: new Text('点击了按钮'),
+        )),
       ),
     );
   }
