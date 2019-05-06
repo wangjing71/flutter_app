c609011d89bf0ae001d557850853704f5b942816
397383523@qq.com <8362781wj>
2019-05-06 11:23:43 +0800

    表单

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,39 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final List<Tab> myTabs = [
+    Tab(
+      text: '选项卡一',
+    ),
+    Tab(
+      text: '选项卡二',
+    ),
+    Tab(
+      text: '选项卡三',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'flutter demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ), //前景色（文本、按钮等）
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to flutter'),
-        ),
-        body: Center(
-          child: new RaisedButton(
-            onPressed: (){
-              
-            },
-            child: new Text('点击了按钮'),
-          )
-        ),
-      ),
+      home: DefaultTabController(
+          length: myTabs.length,
+          child: Scaffold(
+            appBar: AppBar(
+              bottom: TabBar(tabs: myTabs),
+              title: Text('tabBar 示例'),
+
+            ),
+            body: TabBarView(
+                children: myTabs.map((Tab tab){
+                  return Center(child: Text(tab.text),);
+                }).toList(),
+            ),
+          )),
     );
   }
 }
