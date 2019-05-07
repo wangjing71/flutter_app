e0e9e25c7a1147db3715d75c5590c1891af6bfc9
397383523@qq.com <8362781wj>
2019-05-07 14:03:07 +0800

    表单

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,46 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Welcome to flutter'),
         ),
-        body: Center(
-          child: new RaisedButton(
-            onPressed: (){
-              
-            },
-            child: new Text('点击了按钮'),
-          )
-        ),
+        body: Wrap(
+          spacing: 8.0,
+          runSpacing: 4.0,
+          children: <Widget>[
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.grey,
+            ),
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.red,
+            ),
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.black,
+            ),
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.purple,
+            ),
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.pink,
+            ),
+
+
+          ],
+
+
+        )
       ),
     );
   }
