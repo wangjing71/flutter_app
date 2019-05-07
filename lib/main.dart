0572bf248b47f2a4781c27d6476a0150c295b59a
397383523@qq.com <8362781wj>
2019-05-07 10:08:32 +0800

    表单

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'flutter demo',
       theme: ThemeData(
-          brightness: Brightness.light, // 应用 序整体主题的亮度
-          primaryColor: Colors.lightGreen[600], // App 主要部分的背景色
-          accentColor: Colors.orange[600]), //前景色（文本、按钮等）
+          primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to flutter'),
