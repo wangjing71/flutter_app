5f6e37527478667748e0fece44b1a5e9ecf69139
397383523@qq.com <8362781wj>
2019-05-07 10:14:56 +0800

    表单

--- lib/main.dart
+++ lib/main.dart
@@ -8,40 +8,45 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'flutter demo',
       theme: ThemeData(
-          primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to flutter'),
         ),
-        body: Center(
-          child: Container(
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              border: new Border.all(
-                color: Colors.grey,
-                width: 8.0,
-              ),
-              borderRadius:
-                const BorderRadius.all(const Radius.circular(8.0)),
+        body: Container(
 
-            ),
 
-            child: Center(
-              child: Text(
-                'Flutter',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 28.0),
-              ),
-            ),
-          )
+
         ),
+
+//        body: Center(
+//          child: Container(
+//            width: 200,
+//            height: 200,
+//            decoration: BoxDecoration(
+//              color: Colors.red,
+//              border: new Border.all(
+//                color: Colors.grey,
+//                width: 8.0,
+//              ),
+//              borderRadius:
+//                const BorderRadius.all(const Radius.circular(8.0)),
+//
+//            ),
+//
+//            child: Center(
+//              child: Text(
+//                'Flutter',
+//                textAlign: TextAlign.center,
+//                style: TextStyle(fontSize: 28.0),
+//              ),
+//            ),
+//          )
+//        ),
       ),
     );
   }
