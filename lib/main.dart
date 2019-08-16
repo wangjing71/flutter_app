7df3f68d25bf01165906bf9389ea22a307277bd0
397383523@qq.com <8362781wj>
2019-08-16 09:29:40 +0800

    表单

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
             onPressed: (){
               
             },
-            child: new Text('点击了按钮1'),
+            child: new Text('点击了按钮'),
           )
         ),
       ),
